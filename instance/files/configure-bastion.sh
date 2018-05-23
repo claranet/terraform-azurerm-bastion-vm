f7c40a47c515aa17747930349930d54dd2450fd7
Jérémy NANCEL <jeremy.nancel@fr.clara.net>
2018-05-23 14:46:59 +0200

    TER-237 Changed installation method of azure-cli to pip from apt

--- instance/files/configure-bastion.sh
+++ instance/files/configure-bastion.sh
@@ -9,6 +9,7 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' purge salt-minion salt-common
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' update
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install curl ntp python python-pip azure-cli apt-transport-https
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install curl ntp python python-pip apt-transport-https
+sudo pip install azure-cli
 sudo curl -L https://bootstrap.saltstack.com | sudo bash
 sudo salt-call --local --file-root=/srv/salt --pillar-root=/srv/pillar state.highstate
