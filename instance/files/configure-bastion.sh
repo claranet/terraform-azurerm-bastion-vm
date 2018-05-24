b5029607e8144ac7c9a91a70d2df567bb81ea61c
Jérémy NANCEL <jeremy.nancel@fr.clara.net>
2018-05-24 16:25:00 +0200

    TER-237 Remove useless add of MS Debian repo

--- instance/files/configure-bastion.sh
+++ instance/files/configure-bastion.sh
@@ -4,8 +4,6 @@ set -x
 
 sudo mv /tmp/bastion-formula/* /srv/
 sudo rm -rf /tmp/bastion-formula
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' purge salt-minion salt-common
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' update
