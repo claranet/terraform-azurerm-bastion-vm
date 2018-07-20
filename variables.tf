c3a8cdb8892776b9fbd1f6eb5aaed782e5f05c7d
jnancel <jeremy.nancel@fr.clara.net>
2018-07-20 14:55:23 +0200

    TER-237 Changed old VPN IP to new

--- variables.tf
+++ variables.tf
@@ -45,7 +45,7 @@ variable "morea_admin_ips" {
 
   default = [
     "31.3.142.52/32",    # Morea bastion
-    "31.3.136.12/32",    # Morea VPN
+    "185.88.104.18/32",  # Morea VPN
     "78.234.148.111/32", # Morea Coudray
     "62.240.254.57/32",  # Claranet Rennes
   ]
