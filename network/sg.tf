43ae61e5aa456d397561acd0df441a213195d5ea
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2019-01-04 17:56:08 +0100

    AZ-6 azure_region updated to location

--- network/sg.tf
+++ network/sg.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_security_group" "bastion" {
   name                = "sg.${var.environment}.bastion"
-  location            = "${var.azurerm_region}"
+  location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 
   tags = "${merge(local.bastion_tags, var.custom_tags)}"
