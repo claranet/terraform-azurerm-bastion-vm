81c347ae589a863a065a1934c45131f8cecf955f
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2019-02-08 15:12:40 +0100

    AZ-49 bump salt config

--- files/set_hostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-MAX_NAME=$(($(getconf HOST_NAME_MAX)-16))
-VM_HOSTNAME=$(echo ${NAME:0:$MAX_NAME}-${IP}| tr -s '.' '-')
-echo "Changing HOSTNAME to $VM_HOSTNAME"
-sudo salt-call --local network.mod_hostname "$VM_HOSTNAME"
-sudo salt-call --local file.append /etc/hosts "$(echo $IP| tr -s '-' '.') $VM_HOSTNAME"
--- templates/pillar.tpl
+++
@@ -1,9 +0,0 @@
-zabbix_proxy:
-  server: omni.morea.fr
-  port: 10051
-  hostname: ${zabbix_hostname}
-zabbix-agent:
-  hostname: ${zabbix_hostname}
-  server: omni.morea.fr
-zabbix_agent:
-  hostname: ${zabbix_hostname}
