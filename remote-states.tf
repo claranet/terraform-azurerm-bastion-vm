58316d74ac4f66be5e7a44e1774697a61eab1fd1
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2019-04-05 10:18:25 +0200

    AZ-49 Remove unused remote state global vars

--- remote-states.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "global_vars" {
-  backend = "s3"
-
-  config {
-    bucket = "s3-terraform-states-eu-west-1-612688033368"
-    key    = "cloudpublic/cloudpublic/global/vars/terraform.state"
-    region = "eu-west-1"
-  }
-}
