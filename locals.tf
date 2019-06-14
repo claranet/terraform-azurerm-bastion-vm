7a9ddf2728349da716dd9f45f2d02b4bea167e13
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-06-14 16:29:28 +0200

    AZ-49: Update tags and default name

--- locals.tf
+++ locals.tf
@@ -1,8 +1,9 @@
 locals {
-  default_basename = "${var.name}-${var.stack}-${var.client_name}-${var.location_short}-${var.environment}"
+  default_basename = "bastion-${var.stack}-${var.client_name}-${var.location_short}-${var.environment}"
 
   bastion_tags = {
-    env   = "${var.environment}"
-    stack = "bastion"
+    env    = "${var.environment}"
+    stack  = "${var.stack}"
+    module = "bastion"
   }
 }
