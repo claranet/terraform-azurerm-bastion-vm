2351dd8652dd204ee1422efebb60bba7f5da21f4
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-07-10 17:36:33 +0200

    AZ-236: Update variables/ouputs with new modules versions

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,36 @@
 output "bastion_network_public_ip" {
   description = "Bastion public ip"
-  value       = azurerm_public_ip.bastion.ip_address
+  value       = module.bastion-vm.vm_public_ip_address
+}
+
+output "bastion_public_domain_name_label" {
+  description = "Bastion public DNS"
+  value       = module.bastion-vm.vm_public_domain_name_label
 }
 
 output "bastion_network_public_ip_id" {
-  description = "Bastion public ip id"
-  value       = azurerm_public_ip.bastion.id
+  description = "Bastion public ip ID"
+  value       = module.bastion-vm.vm_public_ip_id
 }
 
 output "bastion_network_interface_id" {
   description = "Bastion network interface id"
-  value       = azurerm_network_interface.bastion.id
+  value       = module.bastion-vm.vm_nic_id
 }
 
 output "bastion_network_interface_private_ip" {
   description = "Bastion private ip"
-  value       = azurerm_network_interface.bastion.private_ip_address
+  value       = module.bastion-vm.vm_private_ip_address
 }
 
 output "bastion_virtual_machine_id" {
   description = "Bastion virtual machine id"
-  value       = azurerm_virtual_machine.bastion_instance.id
+  value       = module.bastion-vm.vm_id
 }
 
 output "bastion_virtual_machine_name" {
   description = "Bastion virtual machine name"
-  value       = azurerm_virtual_machine.bastion_instance.name
+  value       = module.bastion-vm.vm_name
 }
 
 output "bastion_admin_username" {
@@ -35,25 +40,10 @@ output "bastion_admin_username" {
 
 output "bastion_virtual_machine_size" {
   description = "Bastion virtual machine size"
-  value       = azurerm_virtual_machine.bastion_instance.vm_size
+  value       = var.vm_size
 }
 
 output "bastion_hostname" {
   description = "Bastion hostname"
   value       = local.hostname
-}
-
-output "bastion_storage_image_reference" {
-  description = "Bastion storage image reference object"
-  value       = azurerm_virtual_machine.bastion_instance.storage_image_reference
-}
-
-output "bastion_storage_os_disk" {
-  description = "Bastion storage OS disk object"
-  value       = azurerm_virtual_machine.bastion_instance.storage_os_disk
-}
-
-output "bastion_public_domain_name_label" {
-  description = "Bastion public DNS"
-  value       = azurerm_public_ip.bastion.domain_name_label
 }
--- r-vm-ansible.tf
+++ r-vm-ansible.tf
@@ -2,8 +2,8 @@ data "template_file" "ansible_inventory" {
   template = file("${path.module}/playbook-ansible/host_ini.tpl")
 
   vars = {
-    vm_fullname = azurerm_virtual_machine.bastion_instance.name
-    vm_ip       = azurerm_public_ip.bastion.ip_address
+    vm_fullname = module.bastion-vm.vm_name
+    vm_ip       = module.bastion-vm.vm_public_ip_address
     vm_user     = var.admin_username
   }
 }
@@ -14,14 +14,12 @@ resource "local_file" "rendered_ansible_inventory" {
 }
 
 resource "null_resource" "ansible_bootstrap_vm" {
-  depends_on = [azurerm_virtual_machine.bastion_instance]
-
   triggers = {
-    uuid = azurerm_virtual_machine.bastion_instance.id
+    uuid = module.bastion-vm.vm_id
   }
 
   provisioner "local-exec" {
-    command = "ansible-galaxy install -r requirements.yml --force && ansible-playbook --private-key=${var.private_key_path} main.yml -e ansible_cloud_provider=azure -e hostname=${azurerm_virtual_machine.bastion_instance.name}-${replace(azurerm_public_ip.bastion.ip_address, ".", "-")}"
+    command = "ansible-galaxy install -r requirements.yml --force && ansible-playbook --private-key=${var.private_key_path} main.yml -e ansible_cloud_provider=azure -e hostname=${module.bastion-vm.vm_name}-${replace(module.bastion-vm.vm_public_ip_address, ".", "-")}"
 
     working_dir = "${path.module}/playbook-ansible"
   }
