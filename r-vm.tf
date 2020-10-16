17dd21f1ed96d21cc40f51d41bf1f53741021f0b
frederic peraud <frederic.peraud@fr.clara.net>
2020-10-16 14:41:56 +0200

    AZ-321: Allow use of pubip_extra_tags and ani_extra_tags

--- r-vm.tf
+++ r-vm.tf
@@ -39,5 +39,7 @@ module "bastion-vm" {
   os_disk_size_gb = var.storage_os_disk_size_gb
   os_disk_type    = var.storage_os_disk_managed_disk_type
 
-  extra_tags = merge(local.bastion_tags, var.bastion_extra_tags)
+  extra_tags           = merge(local.bastion_tags, var.bastion_extra_tags)
+  public_ip_extra_tags = merge(local.bastion_tags, var.pubip_extra_tags)
+  nic_extra_tags       = merge(local.bastion_tags, var.ani_extra_tags)
 }
